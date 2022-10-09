42000deb5bfaf76817e7d22614eada67e33332f1
Daniel Kuck-Alvarez <dankuck@gmail.com>
2022-10-08 22:28:38 -0400

    Step 2: Move code to modules

--- main.py
+++ main.py
@@ -1,47 +1,6 @@
+from to_string import board_to_string, compass_to_string
+
 print("DANDELIONS")
-
-def board_to_string(board):
-    line = ' - - - - - '
-    return line + "\n" + ("\n" + line + "\n").join(list(map(lambda row: '|' + (' '.join(row)) + '|', board))) + "\n" + line
-
-def compass_to_string(compass):
-    out = ''
-    if (compass['nw']):
-        out += '\\'
-    else:
-        out += ' '
-    if (compass['no']):
-        out += '|'
-    else:
-        out += ' '
-    if (compass['ne']):
-        out += '/'
-    else:
-        out += ' '
-    out += "\n"
-    if (compass['we']):
-        out += '-'
-    else:
-        out += ' '
-    out += '+'
-    if (compass['ea']):
-        out += '-'
-    else:
-        out += ' '
-    out += "\n"
-    if (compass['sw']):
-        out += '/'
-    else:
-        out += ' '
-    if (compass['so']):
-        out += '|'
-    else:
-        out += ' '
-    if (compass['se']):
-        out += '\\'
-    else:
-        out += ' '
-    return out
 
 board = [
     [' ', ' ', ' ', ' ', ' '],
